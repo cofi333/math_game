002b13dfadb177d39bf9479dd8c636208e21114f
Stefan Buljic <stefanbvts@gmail.com>
2023-12-20 16:30:11 +0100

    merge commit

--- app/src/main/java/com/example/myapplication/ScoreActivity.java
+++ app/src/main/java/com/example/myapplication/ScoreActivity.java
@@ -1,2 +1,22 @@
-package com.example.myapplication;public class ScoreActivity {
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoreActivity extends AppCompatActivity {
+    String apiResponse = "";
+    public TextView response;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+        response = findViewById(R.id.textView3);
+        Intent intent = getIntent();
+
+        apiResponse = intent.getStringExtra("apiResponse");
+        response.setText(apiResponse);
+    }
 }
